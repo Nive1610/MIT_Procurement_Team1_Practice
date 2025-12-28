0dccf92ca6925011488cb871e5bdcf47c45a98a9
SupriyaR <supriyaraja592@gmail.com>
2025-12-28 20:12:44 +0530

    Create config.py

---
+++ .gitignore/config.py
@@ -0,0 +1,6 @@
+# config.py - DO NOT UPLOAD TO GITHUB (Add this file to .gitignore)
+
+# 1. MongoDB Configuration
+# This is the new safe connection string you created
+MONGO_URI = "mongodb+srv://DatabaseUser:StrongTeamPass_2026!@cluster0.eha9hg9.mongodb.net/?appName=Cluster0"
+DB_NAME = "procurement_db"
